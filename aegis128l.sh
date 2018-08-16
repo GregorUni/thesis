cf9913c0b8b7495f9780a4b86f0f74d39f991ac5
GregorUni <gregor.garten@mailbox.tu-dresden.de>
2018-08-16 16:07:52 +0200

    scripts...

--- aegis128l.sh
+++ aegis128l.sh
@@ -1,9 +1,9 @@
 sudo modprobe -r macsec
-sudo rmmod /lib/modules/4.16.16-041616-generic/kernel/crypto/aegis128l.ko
-sudo make -C /lib/modules/4.16.16-041616-generic/build M=$(pwd) aegis128l.ko
-sudo cp aegis128l.ko /lib/modules/4.16.16-041616-generic/kernel/crypto
-sudo insmod /lib/modules/4.16.16-041616-generic/kernel/crypto/aegis128l.ko
+sudo rmmod /lib/modules/$(uname -r)/kernel/crypto/aegis128l.ko
+sudo make -C /lib/modules/$(uname -r)/build M=$(pwd) aegis128l.ko
+sudo cp aegis128l.ko /lib/modules/$(uname -r)/kernel/crypto
+sudo insmod /lib/modules/$(uname -r)/kernel/crypto/aegis128l.ko
 cd ~
-cd /home/test1/linux-4.16.16/drivers/net
-sudo bash imkernel.sh
+#cd /home/test1/linux-4.16.16/drivers/net
+#sudo bash imkernel.sh
 
