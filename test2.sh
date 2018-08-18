bcd5719a659426360b76a5175f0973c1fd8919b0
GregorUni <gregor.garten@mailbox.tu-dresden.de>
2018-08-18 17:08:22 +0200

    update of test2

--- test2.sh
+++ test2.sh
@@ -16,9 +16,9 @@ init() {
 }
 
 init_remote() {
-    echo -e "${GREEN}Init Remote${NC}"
-    killall -q iperf3
-    nohup iperf3 -s > /dev/null 2>&1 &
+ 	echo -e "${GREEN}Init Remote${NC}"
+    ssh root@$REMOTE_IP "killall -q iperf3"
+    ssh root@$REMOTE_IP "nohup iperf3 -s > /dev/null 2>&1 &"
 }
 
 make_info() {
@@ -33,48 +33,92 @@ make_info() {
 
 
 eva_ping() {
-  echo -e "${GREEN}Start RTT Evaluation of $1 with MTU $2${NC}"
+  #echo -e "${GREEN}Start RTT Evaluation of $1 with MTU $2${NC}"
   PING_FILE=$EVA_DIR/final-$FPREFIX-$1-$2-ping.txt
 
   timeout 360 ping -A $3 -c 50 -s $((($2 - 8 )))  >> $PING_FILE
 }
 
+
 eva_iperf() {
-	echo -e "${GREEN}Start Bandwith Evaluation of $2 with MTU $3${NC}"
-	echo -e "Erster Parameter:$1 Zweiter:$2 Dritter:$3 Vierter:$4"
-	BANDWIDTH_FILE=$EVA_DIR/final-$FPREFIX-$1-$2-iperf.json
+    echo -e "${GREEN}Start Bandwith Evaluation of $2 with MTU $3${NC}"
+    BANDWIDTH_FILE=$EVA_DIR/final-$FPREFIX-$1-$2-iperf.json
 
-	echo -n "[" > $BANDWIDTH_FILE # Clear file
+    echo -n "[" > $BANDWIDTH_FILE # Clear file
 
-	for i in `seq 1 $1`; do
-        	echo -e "Start iperf3 #$i"
-		timeout 20 iperf3 -Jc $4 -V  >> $BANDWIDTH_FILE
-		if [ $? -ne 0 ]; then
-		echo -e "${RED}iperf3 error${NC}"
-	 	#exit 1
-		fi
+    for i in `seq 1 $1`; do
+        echo -e "Start iperf3 #$i"
+        timeout 20 iperf3 -Jc $4 -b0 -V >> $BANDWIDTH_FILE
+        #iperf3 -c 1.1.1.1 -b 0 -V
+        if [ $? -ne 0 ]; then
+            echo -e "${RED}iperf3 error${NC}"
+            #exit 1
+        fi
 
-		if [ $1 -eq $i ]; then
-			echo -ne "]" >> $BANDWIDTH_FILE
-        	else
-            		echo -ne "," >> $BANDWIDTH_FILE
-       		fi;
-    	done
+        if [ $1 -eq $i ]; then
+            echo -ne "]" >> $BANDWIDTH_FILE
+        else
+            echo -ne "," >> $BANDWIDTH_FILE
+        fi;
+    done
 }
+	
 
 eva() {
 	 echo -e "Erster Parameter:$1 Zweiter:$2 Dritter:$3 Vierter:$4 Fünfter: $5"
-	if [[ $5 == m ]]; then
+	if [[ $5 == mwe ]]; then
 		echo -e "Ich war hier"
 		IP=$DEST_IP
+		ssh root@$REMOTE_IP "sh /home/test-2/config_macsec_without_encryption.sh"
                 config_macsec_without_encryption
                 make_info $2 $4
-                eva_ping $2 $4 $IP
-                eva_iperf $1 $2 $4 $IP
+               	eva_ping $2 $4 $IP
+                eva_iperf $1 $2 $4 $DEST_IP
+	
+
+	elif [[ $5 == med ]]; then
+		IP=$DEST_IP
+		ssh root@$REMOTE_IP "sh /home/test-2/config_macsec_encryption_default.sh"
+		config_macsec_encryption_default
+		make_info $2 $4
+		eva_ping $2 $4 $IP
+		eva_iperf $1 $2 $4 $DEST_IP
+
+	elif [[ $5 == cwe ]]; then
+		IP=$DEST_IP
+		ssh root@$REMOTE_IP "sh /home/test-2/config_macsec_chacha_without_encryption.sh"
+		config_macsec_chacha_without_encryption
+		make_info $2 $4
+		eva_ping $2 $4 $IP
+		eva_iperf $1 $2 $4 $DEST_IP
+
+	elif [[ $5 == mce ]]; then
+		IP=$DEST_IP
+		ssh root@$REMOTE_IP "sh /home/test-2/config_macsec_chacha_encryption.sh"
+		config_macsec_chacha_without_encryption
+		make_info $2 $4
+		eva_ping $2 $4 $IP
+		eva_iperf $1 $2 $4 $DEST_IP
+
+	elif [[ $5 == awe ]]; then
+		IP=$DEST_IP
+		ssh root@$REMOTE_IP "sh /home/test-2/config_macsec_aegis128l_without_encryption.sh"
+		config_macsec_aegis128l_without_encryption
+		make_info $2 $4
+		eva_ping $2 $4 $IP
+		eva_iperf $1 $2 $4 $DEST_IP
+
+	elif [[ $5 == mmwe ]]; then
+		IP=$DEST_IP
+		ssh root@$REMOTE_IP "sh /home/test-2/config_macsec_morus640_without_encryption.sh"
+		config_macsec_morus640_without_encryption
+		make_info $2 $4
+		eva_ping $2 $4 $IP
+		eva_iperf $1 $2 $4 $DEST_IP
 
 
-	else
-                IP=$REMOTE_IP
+	else 
+	        IP=$REMOTE_IP
                 make_info $2 $4
                 eva_ping $2 $4 $IP
                 eva_iperf $1 $2 $4 $IP
@@ -87,15 +131,104 @@ config_macsec_without_encryption()
 	sudo modprobe -r macsec
 	sudo modprobe -v macsec
 	sudo ip link add link enp0s3 macsec0 type macsec
-	sudo ip macsec add macsec0 tx sa 0 pn 1 on key 01 12345678901234567890123456789012
-	sudo ip macsec add macsec0 rx address 08:00:27:3d:67:28  port 1
-	sudo ip macsec add macsec0 rx address 08:00:27:3d:67:28  port 1 sa 0 pn 1 on key 02 09876543210987654321098765432109
+	sudo ip macsec add macsec0 tx sa 0 pn 1 on key 01 12345678901234567890123456789012	
+	sudo ip macsec add macsec0 rx address 08:00:27:19:58:33 port 1
+	sudo ip macsec add macsec0 rx address 08:00:27:19:58:33 port 1 sa 0 pn 1 on key 02 09876543210987654321098765432109
 	sudo ip link set dev macsec0 up
 	sudo ifconfig macsec0 10.10.12.1/24
+	sudo ip link set macsec0 type macsec encrypt off
+
+
+}
+
+config_macsec_encryption_default()
+{
+	sudo modprobe -r macsec
+	sudo modprobe -v macsec
+	sudo ip link add link enp0s3 macsec0 type macsec
+	sudo ip macsec add macsec0 tx sa 0 pn 1 on key 01 12345678901234567890123456789012
+	sudo ip macsec add macsec0 rx address 08:00:27:19:58:33 port 1
+	sudo ip macsec add macsec0 rx address 08:00:27:19:58:33 port 1 sa 0 pn 1 on key 02 09876543210987654321098765432109
+	sudo ip link set dev macsec0 up
+	sudo ifconfig macsec0 10.10.12.1/24
+	sudo ip link set macsec0 type macsec encrypt on
+}
+
+config_macsec_chacha_without_encryption()
+{
+	sudo modprobe -r macsec
+	sudo modprobe -v macsec
+	sudo modprobe -v chacha20poly1305
+	sudo ip link add link enp0s3 macsec0 type macsec cipher chacha-poly-256
+	sudo ip macsec add macsec0 tx sa 0 pn 1 on key 01 12345678901234567890123456789012
+	sudo ip macsec add macsec0 rx address 08:00:27:19:58:33 port 1
+	sudo ip macsec add macsec0 rx address 08:00:27:19:58:33 port 1 sa 0 pn 1 on key 02 09876543210987654321098765432109
+	sudo ip link set dev macsec0 up
+	sudo ifconfig macsec0 10.10.12.1/24
+	sudo ip link set macsec0 type macsec encrypt off
+}
+
+config_macsec_chacha_encryption()
+{
+	sudo modprobe -r macsec
+	sudo modprobe -v macsec
+	sudo modprobe -v chacha20poly1305
+	sudo ip link add link enp0s3 macsec0 type macsec cipher chacha-poly-256
+	sudo ip macsec add macsec0 tx sa 0 pn 1 on key 01 12345678901234567890123456789012
+	sudo ip macsec add macsec0 rx address 08:00:27:19:58:33 port 1
+	sudo ip macsec add macsec0 rx address 08:00:27:19:58:33 port 1 sa 0 pn 1 on key 02 09876543210987654321098765432109
+	sudo ip link set dev macsec0 up
+	sudo ifconfig macsec0 10.10.12.1/24
+	sudo ip link set macsec0 type macsec encrypt on
+}
+
+config_macsec_aegis128l_without_encryption()
+{
+	sudo modprobe -r macsec
+	
+
+	sudo cd /home/test1/linux-4.16.16/crypto
+	sudo bash aegis128l.sh
+	sudo cd ~
+
+	sudo modprobe -v macsec
+	#sudo modprobe -v aegis128l
+	sudo ip link add link enp0s3 macsec0 type macsec cipher aegis128l-128
+	sudo ip macsec add macsec0 tx sa 0 pn 1 on key 01 12345678901234567890123456789012
+	sudo ip macsec add macsec0 rx address 08:00:27:19:58:33 port 1
+	sudo ip macsec add macsec0 rx address 08:00:27:19:58:33 port 1 sa 0 pn 1 on key 02 09876543210987654321098765432109
+	sudo ip link set dev macsec0 up
+	sudo ifconfig macsec0 10.10.12.1/24
+	sudo ip link set macsec0 type macsec encrypt off
+}
+
+config_macsec_morus640_without_encryption()
+{
+	sudo modprobe -r macsec
+	
+
+	sudo cd /home/test1/linux-4.16.16/crypto
+	sudo bash morus.sh
+	sudo cd ~
+	
+	sudo modprobe -v macsec
+	#sudo modprobe -v morus640
+	sudo ip link add link enp0s3 macsec0 type macsec cipher morus640-128
+	sudo ip macsec add macsec0 tx sa 0 pn 1 on key 01 12345678901234567890123456789012
+	sudo ip macsec add macsec0 rx address 08:00:27:19:58:33 port 1
+	sudo ip macsec add macsec0 rx address 08:00:27:19:58:33 port 1 sa 0 pn 1 on key 02 09876543210987654321098765432109
+	sudo ip link set dev macsec0 up
+	sudo ifconfig macsec0 10.10.12.1/24
+	sudo ip link set macsec0 type macsec encrypt off
 }
 
 
 init
 make_info
-#eva 4 "no-macsec" 1000 1468
-eva 4 "macsec" 1000 1468 m
+eva 4 "no-macsec" 1000 1468
+eva 4 "macsec-aes(gcm)-we" 1000 1468 mwe
+eva 4 "macsec-aes(gcm)-e" 1000 1468 med
+eva 4 "macsec-chachapoly-we" 1000 1468 cwe
+eva 4 "macsec-chachapoly-e" 1000 1468 mce
+eva 4 "macsec-aegis128l-we" 1000 1468 awe
+eva 4 "macsec-morus640-we" 1000	1468 mmwe
