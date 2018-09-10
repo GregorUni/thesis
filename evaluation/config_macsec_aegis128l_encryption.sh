176e9c6b2bf46d6637a8dd1727f4ef9ceb4178ca
GregorUni <gregor.garten@mailbox.tu-dresden.de>
2018-09-10 15:37:21 +0200

    fixes

--- evaluation/config_macsec_aegis128l_encryption.sh
+++ evaluation/config_macsec_aegis128l_encryption.sh
@@ -2,10 +2,10 @@ sudo modprobe -r macsec
 sudo modprobe -v macsec
 #sudo modprobe -v aegis128l
 sudo ip link add link eno1 macsec0 type macsec cipher aegis128l-128
-sudo ip macsec add macsec0 tx sa 0 pn 1 on key 01 12345678901234567890123456789012
-sudo ip macsec add macsec0 rx address ec:b1:d7:4b:bc:fd port 1
-sudo ip macsec add macsec0 rx address ec:b1:d7:4b:bc:fd port 1 sa 0 pn 1 on key 02 09876543210987654321098765432109
+sudo ip macsec add macsec0 tx sa 0 pn 1 on key 02 09876543210987654321098765432109
+sudo ip macsec add macsec0 rx address ec:b1:d7:4b:bc:01 port 1
+sudo ip macsec add macsec0 rx address ec:b1:d7:4b:bc:01 port 1 sa 0 pn 1 on key 01 12345678901234567890123456789012
 sudo ip link set dev macsec0 up
 sudo ip link set dev macsec0 mtu 1514
-sudo ifconfig macsec0 10.10.12.1/24
+sudo ifconfig macsec0 10.10.12.2/24
 sudo ip link set macsec0 type macsec encrypt on
