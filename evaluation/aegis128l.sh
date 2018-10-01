7f620eff100a438b3f9d109cb3b8f00bc9dfeec1
GregorUni <38833793+GregorUni@users.noreply.github.com>
2018-10-01 21:26:53 +0200

    .

--- evaluation/aegis128l.sh
+++
@@ -1,9 +0,0 @@
-sudo modprobe -r macsec
-sudo rmmod /lib/modules/$(uname -r)/kernel/crypto/aegis128l.ko
-sudo make -C /lib/modules/$(uname -r)/build M=$(pwd) aegis128l.ko
-sudo cp aegis128l.ko /lib/modules/$(uname -r)/kernel/crypto
-sudo insmod /lib/modules/$(uname -r)/kernel/crypto/aegis128l.ko
-cd ~
-#cd /home/test1/linux-4.16.16/drivers/net
-#sudo bash imkernel.sh
-
